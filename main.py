395018b1d717061a5d57b1c3c1d3dcfaa5a00d46
kcrong <kcrong@kim82536.pe.kr>
2016-07-20 13:59:15 +0900

    객체지향적으로 리펙토링

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 Writer Kcrong
 
-python3 main.py [Interface] [victim ip]
+python3 main.py [victim ip]
 """
 
 import re
@@ -9,56 +9,81 @@ import subprocess
 from socket import *
 from struct import pack
 
-
-def get_interface_info(ip=False):
-    """
-    아이피를 인자로 받아 해당 아이피를 가진 인터페이스의 이름을 반환
-    :param ip: ip to find interface
-    :return: interface name that has ip
-    """
-
-    name_pattern = "^(\w+)\s"
-    mac_addr_pattern = ".*?HWaddr[ ]([0-9A-Fa-f:]{17})"
-    ip_addr_pattern = ".*?\n\s+inet[ ]addr:((?:\d+\.){3}\d+)"
-    pattern = re.compile("".join((name_pattern, mac_addr_pattern, ip_addr_pattern)),
-                         flags=re.MULTILINE)
-
-    # 정규식을 이용해 ifconfig 명령어 결과를 파싱
-    ifconfig_result = subprocess.check_output("ifconfig").decode()
-    interfaces = pattern.findall(ifconfig_result)
-
-    for name, mac_addr, ip_addr in interfaces:
-        if ip_addr == ip:
-            return name, mac_addr
-
-    # 해당 아이피를 가진 인터페이스가 없으면 False 반환
-    return False
+ARP_REQUEST = 0
+ARP_RECEIVE = 1
 
 
-def packing_ip(ip):
-    """
-    :param ip: ip to packing big-endian
-    :return: packed ip with big-endian
-    """
-    return pack('!4B', *[int(ip) for ip in ip.split('.')])
+class ARP:
+    def __init__(self, victim):
+        self.target_ip = victim
+        self.name, self.mac, self.ip = self._get_my_interface_info()
 
+    def _get_my_interface_info(self):
+        """
+        target_ip 와 연결된 인터페이스의 정보를 가져옴
 
-def get_my_interface_info(target_ip):
-    """
-    :param target_ip: victim's ip address
-    :return: ip address that connect with victim
-    """
-    with socket(AF_INET, SOCK_DGRAM) as s:
-        s.connect((target_ip, 219))  # 219 is ARP port
-        my_ip = s.getsockname()[0]
+        :return: ip address that connect with victim
+        """
+        with socket(AF_INET, SOCK_DGRAM) as s:
+            s.connect((self.target_ip, 219))  # 219 is ARP port
+            my_ip = s.getsockname()[0]
 
-    name, mac = get_interface_info(my_ip)
+        name, mac = self._get_interface_info(my_ip)
 
-    return name, my_ip, mac
+        return name, my_ip, mac
 
+    def _get_interface_info(self, ip=False):
+        """
+        아이피를 인자로 받아 해당 아이피를 가진 인터페이스의 이름을 반환
 
-def get_victim_mac(target_ip):
-    return target_ip
+        :param ip: ip to find interface
+        :return: interface name that has ip
+        """
+
+        name_pattern = "^(\w+)\s"
+        mac_addr_pattern = ".*?HWaddr[ ]([0-9A-Fa-f:]{17})"
+        ip_addr_pattern = ".*?\n\s+inet[ ]addr:((?:\d+\.){3}\d+)"
+        pattern = re.compile("".join((name_pattern, mac_addr_pattern, ip_addr_pattern)),
+                             flags=re.MULTILINE)
+
+        # 정규식을 이용해 ifconfig 명령어 결과를 파싱
+        ifconfig_result = subprocess.check_output("ifconfig").decode()
+        interfaces = pattern.findall(ifconfig_result)
+
+        for name, mac_addr, ip_addr in interfaces:
+            if ip and self.ip == ip_addr:
+                return name, mac_addr
+
+        # 해당 아이피를 가진 인터페이스가 없으면 False 반환
+        return False
+
+    @staticmethod
+    def packing_ip(ip):
+        """
+        우리가 사용하는 String Format ( "123.123.123.123" ) 을 Big-endian 으로 packing 해주는 함수
+
+        :param ip: ip to packing big-endian
+        :return: packed ip with big-endian
+        """
+        return pack('!4B', *[int(ip) for ip in ip.split('.')])
+
+    def send_arp(self, send_type):
+        """
+        send_type 에 따라 target_ip에 arp 패킷을 전송합니다.
+
+        :param send_type: ARP_REQUEST 혹은 ARP_RECEIVE 중 하나. arp 패킷의 종류
+        :return: None. Just send packet
+        """
+
+        s = socket(AF_PACKET, SOCK_RAW, SOCK_RAW)
+
+    def _get_victim_mac(self):
+        """
+        target_ip 에게 ARP Request 를 보내 MAC 주소를 받아옴
+
+        :return: victim's mac address
+        """
+        return self.target_ip
 
 
 def main():
@@ -66,11 +91,10 @@ def main():
     # if 3 != len(sys.argv):
     #     print("Usage: python3 %s [victim_ip]\nEx) python3 main.py eth0 192.168.0.4")
 
-    interface = 'wlan0'  # sys.argv[1]
     victim_ip = '192.168.1.1'
+    arp = ARP(victim_ip)
 
-    name, ip, mac = get_my_interface_info(victim_ip)
-    victim_mac = get_victim_mac(victim_ip)
+    arp.send_arp(ARP_REQUEST)
 
 
 if __name__ == '__main__':
